9e296744206de0d0081acc8293a079c5ac4617bf
Alexey Kovrizhkin <lekovr@gmail.com>
2020-02-09 21:21:27 +0300

    minor improvements

--- times.sh
+++ times.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
 #
-# 
-# jean@tender.pro
-#
 #  run:
 #  nohup bash times.sh &
 
-dest=/opt/dcape/var/log/webhook/monitor/timing.log
-[ -f $dest ] || echo "Stamp,http,https" > $dest
+[ "$MON_URLS" ]  || { echo "ERROR: 'MON_URLS' value is missing"; exit; }
+[ "$MON_NAMES" ] || { echo "ERROR: 'MON_NAMES' value is missing"; exit; }
+[ "$DEST" ]      || { echo "ERROR: 'DEST' value is missing"; exit; }
+[ "$SLEEP" ]     || { echo "ERROR: 'SLEEP' value is missing"; exit; }
+[ "$FLAG" ]      || { echo "ERROR: 'FLAG' value is missing"; exit; }
 
-sleep=2
-flag=times.allow
+# URLS count is a space count+1
+spaces=" ${MON_URLS//[^ ]}"
+[ "$DEBUG" ] && echo "Spaces: ($spaces)"
 
-touch $flag
-while [ -f $flag ] ; do
+# replace spaces with -o /dev/null
+redirs=${spaces// / -o \/dev\/null/}
+[ "$DEBUG" ] && echo "Redirs: ($redirs)"
+
+touch $FLAG
+
+while [ -f $FLAG ] ; do
+  d=$(date +"%F")
+  dest=${DEST/\%F/$d}
+  [ -f $dest ] || echo "Stamp,"$MON_NAMES > $dest
   f=$(date +"%F %H:%M:%S")
-  t=$(curl -w ',%{time_total}' -s -o /dev/null -o /dev/null 'htt{p,ps}://www.tender.pro/date_time.shtml?check=on')
+  [ "$DEBUG" ] && echo "$redirs $MON_URLS"
+  ta=$(curl -w ' %{time_total}' -s $redirs $MON_URLS)
+  tb=${ta//,/.}
+  t=${tb// /,}
   echo "$f$t" >> $dest
-  sleep $sleep
+  [ "$DEBUG" ] && echo "$f$t"
+  sleep $SLEEP
 done
+echo "Done"
